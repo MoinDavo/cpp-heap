3572beac8298f3297ece6082142d4b8cd61e8fc8
MoinDavo <davo.frewin@gmail.com>
2023-10-16 17:37:15 +0200

    fix possible out of bounds err

--- Heap.cpp
+++ Heap.cpp
@@ -14,17 +14,32 @@ void Heap::heapify()
 {
 	for (int i = 0; i < this->content.size(); i++) // work from top to bottom
 	{
+		int leftInd{2 * i + 1}, rightInd{2 * i + 2};
+		bool ignoreLeft{false}, ignoreRight{false};
+
+		// catch index out of bounds
+		if (leftInd > this->content.size() - 1)
+		{
+			ignoreLeft = true;
+			leftInd = this->content.size() - 1;
+		}
+		if (rightInd > this->content.size() - 1) 
+		{
+			ignoreRight = true;
+			rightInd = this->content.size() - 1;
+		}
+
 		HeapElement parent{this->content[i]};
 		HeapElement left{this->content[(2 * i + 1)]};
 		HeapElement right{this->content[(2 * i + 2)]};
 		bool swapped{false};
 
-		if (parent.getKey() < left.getKey())
+		if (parent.getKey() < left.getKey() && !ignoreLeft)
 		{
 			swap(parent, left);
 			swapped = true;
 		}
-		else if (parent.getKey() > right.getKey())
+		else if (parent.getKey() > right.getKey() && !ignoreRight)
 		{
 			swap(parent, right);
 			swapped = true;
