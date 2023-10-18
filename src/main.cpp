17c47019ecaecb3bd8da83c336119a6931172e06
MoinDavo <davo.frewin@gmail.com>
2023-10-18 13:18:59 +0200

    add tests for heap

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
+#include "../include/Heap.h"
+#include <iostream>
+
 int main(int argc, char const *argv[])
 {
-	/* code */
+	Heap h;
+
+	//test if heap works (i/o)
+	h.addElement(HeapElement(5));
+	h.addElement(HeapElement(1));
+	h.addElement(HeapElement(3));
+	h.addElement(HeapElement(4));
+	h.addElement(HeapElement(2));
+
+	std::cout << h.pop().getKey();
+	std::cout << h.pop().getKey();
+	std::cout << h.pop().getKey();
+	std::cout << h.pop().getKey();
+	std::cout << h.pop().getKey();
+
 	return 0;
 }
