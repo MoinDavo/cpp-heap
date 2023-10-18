fdb7f9d4ef3d69962e029b798eed14b2391f24e2
MoinDavo <davo.frewin@gmail.com>
2023-10-18 15:00:01 +0200

    add formatting

--- src/HeapElement.cpp
+++ src/HeapElement.cpp
@@ -1,6 +1,14 @@
 #include "HeapElement.h"
 
+// ----------------------------------------------
+// Constructors:
+// ----------------------------------------------
 HeapElement::HeapElement() : key{0} {};
 HeapElement::HeapElement(int k) : key{k} {};
+HeapElement::~HeapElement() {};
+
+// ----------------------------------------------
+// Public:
+// ----------------------------------------------
 
 int HeapElement::getKey() { return this->key; };
