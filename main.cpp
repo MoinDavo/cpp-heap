c459d7bc7f171f5391d21549ab7454d2a01fa590
MoinDavo <davo.frewin@gmail.com>
2023-10-18 16:23:21 +0200

    start testing

--- main.cpp
+++ main.cpp
@@ -6,20 +6,22 @@ int main(int argc, char const *argv[])
 {
 	Heap h;
 
-	/*
 	//test if heap works (i/o)
+	std::cout << "fill heap...";
 	h.addElement(HeapElement(5));
 	h.addElement(HeapElement(1));
 	h.addElement(HeapElement(3));
 	h.addElement(HeapElement(4));
 	h.addElement(HeapElement(2));
 
-	std::cout << h.pop().getKey();
-	std::cout << h.pop().getKey();
-	std::cout << h.pop().getKey();
-	std::cout << h.pop().getKey();
-	std::cout << h.pop().getKey();
-	*/
+	std::cout << "test pop()...";
+	std::cout << "to expect: \n" << "5, 4, 3, 2, 1"; 
+
+	std::cout << std::to_string(h.pop().getKey()) << ", ";
+	std::cout << std::to_string(h.pop().getKey()) << ", ";
+	std::cout << std::to_string(h.pop().getKey()) << ", ";
+	std::cout << std::to_string(h.pop().getKey()) << ", ";
+	std::cout << std::to_string(h.pop().getKey());
 
 	return 0;
 }
