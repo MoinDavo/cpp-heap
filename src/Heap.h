2f1eb605a698208263ef32e6c80a50598a41bbff
MoinDavo <davo.frewin@gmail.com>
2023-11-02 12:48:45 +0100

    formatting

--- src/Heap.h
+++ src/Heap.h
@@ -11,17 +11,17 @@ private:
 
 	void heapify();							 // sort an already existing heap
 	void heapify2();						 // sort an already existing heap
-	void heapifyRecursive(int);						 // sort an already existing heap from supplied index
+	void heapifyRecursive(int);				 // sort an already existing heap from supplied index
 	void swap(HeapElement &, HeapElement &); // helper method for heapify()
 
 public:
 	Heap();
 	~Heap();
 
-	void addElement(const HeapElement&); // add an element to the heap and call heapify()
-	HeapElement pop();			  // return and remove the top element
-	HeapElement top();			  // return the top element
-	bool isEmpty();				  // return if heap is empty (contains no elements)
+	void addElement(const HeapElement &); // add an element to the heap and call heapify()
+	HeapElement pop();					  // return and remove the top element
+	HeapElement top();					  // return the top element
+	bool isEmpty();						  // return if heap is empty (contains no elements)
 
 	void printHeap();
 };
